24fb64f15e3058c79d17dad948e6a2448f37ad9e
frank.yy <frank.yy@binance.com>
2025-04-26 00:55:54 +0800

    Refactor main.go to improve array and slice examples with additional comments and print statements

--- Lesson 4 - Arrays & Slices/main.go
+++ Lesson 4 - Arrays & Slices/main.go
@@ -4,12 +4,29 @@ import "fmt"
 
 func main() {
 
+	// 1. Arrays
 	// var ages [3]int = [3]int{20, 25, 30}
 	var ages = [3]int{20, 25, 30}
 	fmt.Println("Ages:", ages, ", length: ", len(ages))
 
 	names := [4]string{"John", "Doe", "Smith", "Yoshi"}
 	fmt.Println("Names:", names, ", length: ", len(names))
+	names[1] = "Luigi"
+	fmt.Println("Names:", names, ", length: ", len(names))
 
-	// Slices
+	// 2. Slices (use arrays under the hood)
+	var scores = []int{100, 50, 60}
+	scores[2] = 25
+	scores = append(scores, 85)
+	fmt.Println("Scores:", scores, ", length: ", len(scores))
+
+	// 3. Slice Ranges
+	rangeOne := names[1:3]
+	rangeTwo := names[2:]
+	rangeThree := names[:3]
+	fmt.Println(rangeOne, rangeTwo, rangeThree)
+
+	rangeOne = append(rangeOne, "Koopa")
+	fmt.Println("Range One:", rangeOne, ", length: ", len(rangeOne))
+
 }
